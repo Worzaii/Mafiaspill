a54ab4a5c6d36dbde52cc02d166bdf6bad2a48f8
Worzaii <baretester@live.no>
2020-04-10 16:38:55 +0200

    Going through working scripts vs non-functional ones. Sorting back and forth until everything has been looked through properly.

---
+++ tests/sessions.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+echo "<h1>Hey!</h1>";
+echo "<p>Your session lang is: " . $_SESSION["lang"] . "</p>";
+foreach ($_SESSION as $key => $value) {
+    echo "<p>$key: $value</p>";
+}
+echo "<hr>";
+/* Needs this in php.ini file:
+* extension=intl
+*/
+echo "Closest accepted language is: ".Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+/*foreach ($_SERVER as $key => $value) {
+    echo "<p>$key: $value</p>";
+}*/
