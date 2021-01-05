0706bf334473582fd6d67b9d8d3eeb0e2149e5c0
Mollysaurusen <76933979+Mollysaurusen@users.noreply.github.com>
2021-01-05 18:29:25 +0100

    Add files via upload

---
+++ endreprofil.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * TODO: Make the new crime script working again, but better!
+ */
+
+use UserObject\EndreProfil\EndreProfil;
+
+include("core.php");
+include_once '/classes/MinSide.php';
+$endreprofil = new EndreProfil($obj, $db);
