68565676c01a5d6a9cbbf57a2463f01a7beac86a
Worzaii <worzaii@users.noreply.github.com>
2022-02-17 15:06:29 +0100

    Updated readme.md and index.php to reflect coming changes.

--- index.php
+++ index.php
@@ -8,5 +8,12 @@ use Monolog\Handler\StreamHandler;
 $log = new Logger('mafiagame');
 $log->pushHandler(new StreamHandler('logs/mafia_debug.log', Logger::DEBUG, false));
 $log->info('Starting up');
+/**
+ * Implement the following:
+ * Authentication
+ * Routing
+ * Page Addons depending on Auth
+ * Automatic handlers?
+ */
 
 $log->info('Completed, shutting down');
